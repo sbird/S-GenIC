bb45559faa455027657d4f417dc09c8da3d824d4
Simeon Bird <spb41@cam.ac.uk>
2010-04-26 17:00:18 +0100

    Fix problem where string wasn't completely read

--- read_param.c
+++ read_param.c
@@ -11,9 +11,10 @@ void read_parameterfile(char *fname)
 #define STRING 2
 #define INT 3
 #define MAXTAGS 300
+#define BUFSIZE 400
 
   FILE *fd;
-  char buf[200], buf1[200], buf2[200], buf3[200];
+  char buf[BUFSIZE], buf1[BUFSIZE], buf2[BUFSIZE], buf3[BUFSIZE];
   int i, j, nt;
   int id[MAXTAGS];
   void *addr[MAXTAGS];
@@ -153,7 +154,20 @@ void read_parameterfile(char *fname)
       while(!feof(fd))
 	{
 	  buf[0] = 0;
-	  fgets(buf, 200, fd);
+	  fgets(buf, BUFSIZE, fd);
+          /*Check to see if the buffer was too small: 
+           * If it is large enough, should have a newline at the end*/
+          for(i=0;i<BUFSIZE;i++)
+          {
+             if(buf[i]=='\r' || buf[i]=='\n')
+                     break;
+          }
+          if(buf[BUFSIZE-1]=='\0' && i==BUFSIZE-1){
+                  fprintf(stderr, "Error! Param line buffer not large enough.\n"
+                                  "Edit read_param.c\n"
+                                  "Read was:%s\n",buf);
+                  FatalError(43);
+          }
 
 	  if(sscanf(buf, "%s%s%s", buf1, buf2, buf3) < 2)
 	    continue;
