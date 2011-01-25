94932349811b3f122597dd84cf70e9306d691e79
Simeon Bird <spb41@cam.ac.uk>
2011-01-25 14:56:08 +0000

    Fix bad initialisation of P when GlassTileFac != 1

--- read_glass.cpp
+++ read_glass.cpp
@@ -7,12 +7,13 @@
 
 int64_t read_glass(GadgetReader::GSnap& snap, int type, int GlassTileFac, struct part_data *& P)
 {
-  int i, j, k, n;
   float *pos = NULL;
   size_t bytes;
   int64_t NumPart;
-  int64_t Nglass = snap.GetNpart(type);
-  double GlassBox=snap.GetHeader().BoxSize;
+  const int64_t Nglass = snap.GetNpart(type);
+  const double GlassBox=snap.GetHeader().BoxSize;
+  const int G2Tile = GlassTileFac*GlassTileFac;
+  const int G3Tile = G2Tile*GlassTileFac;
 
   if(!(pos = (float *) malloc(sizeof(float) * Nglass * 3))){
           fprintf(stderr,"failed to allocate %g Mbyte for glass file\n", sizeof(float) * Nglass * 3.0 / (1024.0 * 1024.0));
@@ -25,22 +26,28 @@ int64_t read_glass(GadgetReader::GSnap& snap, int type, int GlassTileFac, struct
           exit(113);
   }
 
-  NumPart = Nglass*GlassTileFac*GlassTileFac*GlassTileFac;
+  NumPart = Nglass*G3Tile;
 
   if(!(P = (struct part_data *) malloc(bytes = sizeof(struct part_data) * NumPart))){
 	printf("Failed to allocate %g Mbyte (%ld particles)\n", bytes / (1024.0 * 1024.0),NumPart);
 	exit(9891);
   }
-
-  for(i = 0; i < GlassTileFac; i++)
-    for(j = 0; j < GlassTileFac; j++)
-      for(k = 0; k < GlassTileFac; k++){
-        for(n = 0; n < Nglass; n++){
-            P[n].Pos[0] = pos[3 * n] / GlassBox * (Box / GlassTileFac) + i * (Box / GlassTileFac);
-            P[n].Pos[1]= pos[3 * n + 1] / GlassBox * (Box / GlassTileFac) + j * (Box / GlassTileFac);
-            P[n].Pos[2]  = pos[3 * n + 2] / GlassBox * (Box / GlassTileFac) + k * (Box / GlassTileFac);
+  
+#pragma omp parallel
+  {
+  
+  #pragma omp for schedule(static)
+  for(int i = 0; i < GlassTileFac; i++)
+    for(int j = 0; j < GlassTileFac; j++)
+      for(int k = 0; k < GlassTileFac; k++){
+        for(int n = 0; n < Nglass; n++){
+            int64_t index = n+k*Nglass+j*Nglass*GlassTileFac+i*G2Tile*Nglass;
+            P[index].Pos[0] = pos[3 * n] / GlassBox * (Box / GlassTileFac) + i * (Box / GlassTileFac);
+            P[index].Pos[1]= pos[3 * n + 1] / GlassBox * (Box / GlassTileFac) + j * (Box / GlassTileFac);
+            P[index].Pos[2]  = pos[3 * n + 2] / GlassBox * (Box / GlassTileFac) + k * (Box / GlassTileFac);
         }
       }
+  }
 
   free(pos);
   printf("Type %d has %ld particles\n",type,NumPart);
