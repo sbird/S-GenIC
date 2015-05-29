d8926dc3d2e0b60ba2e48be80fa4d7fba8414369
Simeon Bird <sbird@andrew.cmu.edu>
2015-05-29 18:02:13 -0400

    More tests for power spectrum

--- test.cpp
+++ test.cpp
@@ -101,6 +101,35 @@ BOOST_AUTO_TEST_CASE(check_power_spec_camb)
     std::string matpow2 = "testdata/cambv2_matterpow_99.dat";
     PowerSpec_Tabulated pspec2(trans2.c_str(), matpow2.c_str(), 0.233+0.0463, 0.7331, 0.0463,0.,3.085678e24, 3.085678e21, false, false);
     BOOST_CHECK_EQUAL(pspec2.size(),336);
+    //Check that the tabulated power spectrum gives the right answer
+    //First check ranges: these should both be out of range.
+    //Should be the same k as in the file (but /10^3 for Mpc -> kpc)
+    BOOST_CHECK_EQUAL(pspec.power(9.8e-9,6), 0.);
+    BOOST_CHECK_EQUAL(pspec.power(2.1e-1,6), 0.);
+    //Now check total power: k divided by 10^3,
+    //Conversion for P(k) is 10^9/(2pi)^3
+    BOOST_CHECK_CLOSE_FRACTION(pspec.power(0.11239E-01/1e3,6), 0.53091E+01*1e9/(pow(2*M_PI,3)),1e-6);
+    BOOST_CHECK_CLOSE_FRACTION(pspec.power(0.10022E+01/1e3,6), 0.13121E-01*1e9/(pow(2*M_PI,3)),1e-6);
+    //Check that it gives reasonable results when interpolating
+    for (int k = 1; k < 100; k++) {
+        double newk = 0.10022E+01/1e3+ k*(0.10362E+01-0.10022E+01)/1e3/100;
+        BOOST_CHECK_LT(pspec.power(newk,6), 0.13121E-01*1e9/(pow(2*M_PI,3)));
+        BOOST_CHECK_GT(pspec.power(newk,6), 0.12154E-01*1e9/(pow(2*M_PI,3)));
+    }
+    //Now check transfer functions: ratio of total to species should be ratio of T_s to T_tot squared: large scales where T~ 1
+    BOOST_CHECK_CLOSE_FRACTION(pspec.power(0.208152E-02/1e3,0)/pspec.power(0.208152E-02/1e3,6), pow(0.255362E+06/0.255697E+06,2),1e-6);
+    //CDM
+    BOOST_CHECK_CLOSE_FRACTION(pspec.power(0.208152E-02/1e3,1)/pspec.power(0.208152E-02/1e3,6), pow(0.255765E+06/0.255697E+06,2),1e-6);
+    //massless NU
+    BOOST_CHECK_CLOSE_FRACTION(pspec.power(0.208152E-02/1e3,3)/pspec.power(0.208152E-02/1e3,6), pow(0.274272E+06/0.255697E+06,2),1e-6);
+    //Massive nu (which are zero here)
+    BOOST_CHECK_CLOSE(pspec.power(0.208152E-02/1e3,2), 0,1e-6);
+    //Small scales where there are differences
+    //T_tot=0.255697E+06
+    //Baryons
+    BOOST_CHECK_CLOSE_FRACTION(pspec2.power(0.111030E+00/1e3,0)/pspec2.power(0.111030E+00/1e3,6), pow(0.200504E+05/0.277947E+05,2),1e-6);
+    //CDM
+    BOOST_CHECK_CLOSE_FRACTION(pspec2.power(0.111030E+00/1e3,1)/pspec2.power(0.111030E+00/1e3,6), pow(0.293336E+05/0.277947E+05,2),1e-6);
 }
 
 class TestFermiDirac: public FermiDiracVel
